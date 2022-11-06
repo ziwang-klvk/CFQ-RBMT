9c9928abba8cd26366f3fa31252c476e5ba71e0a
ziwang <wangziwz123@gmail.com>
2022-11-07 00:20:27 +0100

    The generator now generate meta data file

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,28 +1,30 @@
 import re
 import os
+import json
 from tqdm import tqdm
 
-import urbans
+import urbans.urbans as urbans
 from grammar.dictionary import dic_en2jp
 from grammar.source_grammar import EN_source_transduction_grammar as src_grammar
 from grammar.transduction_rule import src_to_target_grammar
 from grammar.postproc import post_processing, pref_pattern
 
 
-ROOT_PATH = "mcwq/official"
+ROOT_PATH = "pipeline/mcwq/official"
 SAVE_PATH = "jcwq"
 
-def pipeline(sentences: list or str) -> list:
+def pipeline(sentences: list or str):
     post_processor = post_processing()
 
     e2j_translator = urbans.Translator(src_grammar=src_grammar,
                                        src_to_tgt_grammar=src_to_target_grammar,
                                        src_to_tgt_dictionary=dic_en2jp,
+                                       tracker=True
                                        )
 
     translation, trans_map = e2j_translator.translate(sentences, remove_space=True, prefered_pattern=pref_pattern)
     translation = post_processor.replace(translation)
-    return translation
+    return translation, trans_map
 
 
 def splitIO(line: str) -> list:
@@ -45,7 +47,7 @@ def translate_file_en2jp(root_path: str, save_path: str) -> None:
     :param save_path: path to a folder to save the new dataset if existing and will create the directory otherwise
     the file name should be xxx.en.txt
     """
-
+    text_meta = {}
     for path, dir_list, file_list in os.walk(root_path):
         for file_name in file_list:
             if '.en.' in file_name:
@@ -53,23 +55,48 @@ def translate_file_en2jp(root_path: str, save_path: str) -> None:
                 save_file_path = org_file_path.replace(root_path, save_path).replace('.en.', '.ja.')
                 # make the directory if not existing
                 os.makedirs(os.path.dirname(save_file_path), exist_ok=True)
-
                 num_lines = sum(1 for line in open(org_file_path, 'r'))
+
+
+
                 with open(org_file_path, 'r') as fr, open(save_file_path, 'w') as fw:
                     print(f'Translating {org_file_path}...')
+
                     for line in tqdm(fr, total=num_lines):
                         text2sparql = splitIO(line)
-                        jp_text = pipeline(text2sparql[0])[0]
+                        en_text = text2sparql[0]
+                        jp_text, trans_map = pipeline(en_text)
+                        jp_text = jp_text[0]
                         jp_text2sparql = [jp_text, text2sparql[1]]
                         jp_line = 'IN: ' + jp_text2sparql[0] + '  OUT: ' + jp_text2sparql[1]
                         fw.write(jp_line)
+
+                        # record meta data
+                        line_meta = trans_map[en_text]
+                        line_meta['source'] = line
+
+                        text_meta[jp_text] = line_meta
+
+
                     print(f'Completed {org_file_path} translation, generated {save_file_path}!\n')
             else:
                 continue
+        
+    # save meta data
+    with open(os.path.join(save_path, 'meta.json'), 'w') as fmeta:
+        json.dump(text_meta, fmeta)
+
+def save_rule_set(save_path: str):
+    rule_set = {'source_grammar':src_grammar, 
+            'transduction_rule':src_to_target_grammar,
+            'dictionary':dic_en2jp}
+    with open(os.path.join(save_path, 'meta_rule_set.json'), 'w') as frule:
+        json.dump(rule_set, frule)
 
 
 def generator():
     translate_file_en2jp(root_path=ROOT_PATH, save_path=SAVE_PATH)
+    save_rule_set(save_path=SAVE_PATH)
 
 if __name__ == "__main__":
     generator()
